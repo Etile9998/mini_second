4df857ee1d9c0eaf842837947b2568718dd04249
Haan0819 <119270520+Haan0819@users.noreply.github.com>
2023-02-03 10:57:24 +0900

    찐찐찐찐최종본

--- webapp/static/webapp/assets/js/kr_runtime.js
+++ webapp/static/webapp/assets/js/kr_runtime.js
@@ -1,6 +1,29 @@
 let p_run_time = document.querySelector('.runtime_info')
 let accor_runtime = document.querySelectorAll('#accor_runtime')
 
+if (p_run_time != null){
+    let second = parseInt(parseInt(p_run_time.innerText) / 1000);
+    let minute = parseInt(second / 60);
+    second = second % 60;
+    let hour   = parseInt(minute / 60);
+    minute = minute % 60;
+
+    let result = '';
+    if (hour > 0){
+       result += hour + '시간 ';
+    }
+    if (minute > 0){
+       result += minute + '분 ';
+    }
+    if (second > 0){
+       result += second + '초';
+    }
+    if (!second && !minute && !hour){
+      result = 'NULL';
+    }
+    p_run_time.innerText = result;
+    }
+
 for (let i = 0 ; i < accor_runtime.length ; i++){
     let second = parseInt(parseInt(accor_runtime[i].innerText) / 1000);
     let minute = parseInt(second / 60);
@@ -21,7 +44,6 @@ for (let i = 0 ; i < accor_runtime.length ; i++){
     if (!second && !minute && !hour){
       result = 'NULL';
     }
-    if (i == 0 )p_run_time.innerText = result;
     if(accor_runtime[i] != null){
         accor_runtime[i].innerText = '구동시간 : ' + result;
     }
